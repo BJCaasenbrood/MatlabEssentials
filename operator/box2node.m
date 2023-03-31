b5cb8d2a257f9b438b1a8f38e3f8238b193f1de5
BJCaasenbrood <b.j.caasenbrood@gmail.com>
2023-03-31 15:34:29 +0200

    added box2node.m

--- operator/box2node.m
+++ operator/box2node.m
@@ -1,12 +1,12 @@
-function Nodes = box2node(B)
+function out = box2node(B)
 
 if numel(B) == 4
-    Nodes = [B(1),B(3);
+    out = [B(1),B(3);
         B(2),B(3);
         B(2),B(4);
         B(1),B(4)];
 else
-    Nodes = [B(1),B(3),B(5);
+    out = [B(1),B(3),B(5);
         B(1),B(3),B(6);
         B(1),B(4),B(5);
         B(1),B(4),B(6);
