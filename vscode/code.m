021cdc5c1fe49fb3787f114237685b487d32fdd9
BJCaasenbrood <b.j.caasenbrood@gmail.com>
2023-04-05 09:32:59 +0200

    updated code.m -- a visual studio code interface for matlab

--- vscode/code.m
+++ vscode/code.m
@@ -1,4 +1,27 @@
+% CODE Opens VSCode editor to a specific file and line number.
+%
+%   CODE FILE opens Visual Studio Code to the specified FILE.
+%
+%   CODE FILE LINE opens Visual Studio Code to the specified FILE at LINE
+%   number specified by the user (default = 0).
+%
+%   Example:
+%       code myFile will open 'myFile.m' in Visual Studio Code and
+%       place the cursor at line number 10.
+%
+%   Note:
+%       This function requires that the 'code' command is available in the
+%       system path. If you are using a different editor or IDE, you may need
+%       to modify this function to use the appropriate command.
+%
+% See also WHEREIS.
+
 function code(file, line)
+
+    % specify your editors symbolic path here
+    editorpath = 'code';
+    assert(nargin>1, 'CODE requires atleast a file name');
+
     if nargin < 2
         addline = '';
     else
@@ -6,6 +29,6 @@ function code(file, line)
     end
     
     filepath = whereis(file);
-    command = ['code --goto "', filepath, addline,'"'];
+    command = [editorpath,' --goto "', filepath, addline,'"'];
     [status, cmdout] = system(command);
 end
