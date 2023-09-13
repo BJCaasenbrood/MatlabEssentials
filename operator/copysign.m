7c31cf0443e4318a56b2c930a5a3c05ce10c21fc
BJCaasenbrood <b.j.caasenbrood@gmail.com>
2023-09-13 09:40:13 +0200

    Refactor copysign and fquiver functions
    - Simplified the copysign function by removing unnecessary assertion and using element-wise multiplication instead of subtraction
    - Modified the fquiver function to handle both 2D and 3D input arrays, resulting in a more concise code

--- operator/copysign.m
+++ operator/copysign.m
@@ -1,6 +1,4 @@
 function y = copysign(a,b)
-    assert(numel(a) == numel(b), 'a and b must be of same size');
-
     I = b >= 0;        
-    y = abs(a) * (I - 1) - abs(a) * I;
+    y = abs(a) .* I - abs(a) .* (1-I);
 end
