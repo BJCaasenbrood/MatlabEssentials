723598522b5e1065f38c7d8b2f4285117777a376
BJCaasenbrood <b.j.caasenbrood@gmail.com>
2023-09-13 15:31:03 +0200

    Add planeproject.m function for projecting a vector onto a plane
    This commit adds the planeproject.m function, which takes in three input vectors and projects a given vector onto the plane defined by the first two input vectors. It checks if the input vectors have the correct dimensions and handles degenerate cases. The function then calculates the normal vector of the plane and projects the given vector onto it using dot product.

---
+++ operator/planeproject.m
@@ -0,0 +1,18 @@
+function Y = planeproject(n1, n2, z)
+    % Check if the input vectors have the correct dimensions
+    if ~isequal(size(n1), size(n2), size(z))
+        error('Input vectors must have the same dimensions.');
+    end
+    
+    % Calculate the normal vector of the plane
+    Nv = cross(n1, n2);
+    
+    if all(Nv == 0)
+        error('The plane defined by the input vectors is degenerate.');
+    else
+        N = N / norm(Nv);
+    end
+    
+    % Project the vector onto the plane
+    Y = z - dot(z, normalVec) * normalVec;
+end
